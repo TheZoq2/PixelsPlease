12a4df2c4d3c47b30f257abf246f81fcb23472c7
TheZoq2 <frans.skarman@gmail.com>
2017-05-06 17:49:31 +0200

    Create gamestate class

--- main.py
+++ main.py
@@ -7,52 +7,59 @@ import generator
 
 resolution = (1024, 768)
 
+class GameState:
+    def __init__(self):
+        self.censor_texture = None
+        self.censor_texture_sprite = None
+        self.map = None
+        self.page = None
+        self.paper = None
+
 def main():
+    state = GameState()
+
     window = sf.RenderWindow(sf.VideoMode(resolution[0], resolution[1]), "Pixels please")
     #window.clear(sf.Color.BLUE)
 
-    censor_texture = sf.RenderTexture(resolution[0], resolution[1])
-    censor_texture.clear(sf.Color.TRANSPARENT)
-    censor_texture_sprite = sf.Sprite(censor_texture.texture)
-
-    rekt = sf.RectangleShape();
-    rekt.size = resolution
-    rekt.fill_color = sf.Color(255, 255, 255)
+    state.censor_texture = sf.RenderTexture(resolution[0], resolution[1])
+    state.censor_texture.clear(sf.Color.TRANSPARENT)
+    state.censor_texture_sprite = sf.Sprite(state.censor_texture.texture)
 
     shader = sf.Shader.from_file("media/shaders/censor.vert", "media/shaders/censor.frag")
 
     #paper = sf.Texture.from_file('media/images/pixels_please_paper_1.png')
-    map  = sf.Texture.from_file('media/images/pixels_please_paper_1_censor_map.png')
-    page = get_page()
-    paper = sf.Texture.from_image(page.get_image())
+    state.map  = sf.Texture.from_file('media/images/pixels_please_paper_1_censor_map.png')
+    state.page = get_page()
+    state.paper = sf.Texture.from_image(state.page.get_image())
 
 
     while True:
         #time.sleep(0.001) # If you remove this your computer might freze
 
 
-        censor_texture.display()
-        window.draw(rekt)
-        window.draw(sf.Sprite(paper))
-        window.draw(censor_texture_sprite, sf.RenderStates(shader=shader))
+        state.censor_texture.display()
+        window.draw(sf.Sprite(state.paper))
+        window.draw(state.censor_texture_sprite, sf.RenderStates(shader=shader))
 
-        for a in page.articles:
+        for a in state.page.articles:
             window.draw(a.get_text())
 
         window.display()
 
         for event in window.events:
-            rt = event_handler.check_event(window, event, censor_texture)
+            rt = event_handler.check_event(window, event, state.censor_texture)
             if rt == "END":
-                end_censor(censor_texture.texture, map)
+                end_censor(state)
 
 
-def end_censor(censor, map):
-    per_people, per_goverment = image_handler.compare_images(map.to_image(), censor.to_image())
+def end_censor(state):
+    per_people, per_goverment = image_handler.compare_images(state.map.to_image(), state.censor.to_image())
     # save the score
     # next page
     print("PEOPLE SCORE: "+str(per_people)) # debug
     print("GOV SCORE: "+str(per_goverment)) # debug
+
+    #generate a new game state
 
 
 def get_page():
